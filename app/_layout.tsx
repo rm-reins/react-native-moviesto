89f4212e468b2cd4b5ffc6f9b6fe56e9816f5523
rm-reins <rm_reins@icloud.com>
2025-11-25 01:03:55 +0100

    feat: wrap layout in SafeAreaProvider for improved safe area handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
