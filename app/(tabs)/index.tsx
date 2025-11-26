d27317c858638f6b790a3a3e4939bddc6751632b
rm-reins <rm_reins@icloud.com>
2025-11-26 15:09:50 +0100

    feat: implement consistent layout with LinearGradient and ScrollView across tab screens

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,33 @@
 import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
   const insets = useSafeAreaInsets();
 
   return (
-    <LinearGradient
-      colors={["#030014", "#A8B5DB"]}
-      start={{ x: 1, y: 0 }}
-      end={{ x: 1, y: 1 }}
-      style={{ flex: 1, paddingTop: insets.top }}
-    />
+    <View className="flex-1">
+      <StatusBar style="light" />
+
+      <LinearGradient
+        colors={["#030014", "#A8B5DB"]}
+        start={{ x: 1, y: 0 }}
+        end={{ x: 2, y: 2 }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      />
+
+      <ScrollView
+        className="flex-1"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: insets.top }}
+      ></ScrollView>
+    </View>
   );
 }
