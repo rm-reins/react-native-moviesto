9c3c95479a7e342ae569ee037dbb8480d404485e
rm-reins <rm_reins@icloud.com>
2025-12-11 00:36:05 +0100

    feat: integrate trending movies feature by fetching and displaying trending movies alongside latest movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
-import { MovieCard } from "@/components";
+import { MovieCard, TrendingCard } from "@/components";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/hooks/useFetch";
 import { LinearGradient } from "expo-linear-gradient";
-import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { ActivityIndicator, FlatList, ScrollView, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
   const insets = useSafeAreaInsets();
-  const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingMoviesLoading,
+    error: trendingMoviesError,
+  } = useFetch(getTrendingMovies);
+
   const {
     data: movies,
     loading: moviesLoading,
@@ -38,15 +44,46 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingTop: insets.top }}
       >
-        {moviesLoading ? (
+        {moviesLoading || trendingMoviesLoading ? (
           <ActivityIndicator size="large" color="#A8B5DB" />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingMoviesError ? (
+          <View className="m-5 px-2 gap-2">
+            {moviesError && (
+              <Text className="text-red-400 text-xl">
+                Error loading latest movies: {moviesError.message}
+              </Text>
+            )}
+            {trendingMoviesError && (
+              <Text className="text-red-400 text-xl">
+                Error loading trending movies: {trendingMoviesError.message}
+              </Text>
+            )}
+          </View>
         ) : (
           <View className="flex-1 mt-5 px-4">
             <Text className="text-3xl text-accent font-bold text-center">
               Welcome to Moviesto!
             </Text>
+
+            {trendingMovies && (
+              <View>
+                <Text className="text-lg text-white font-bold mt-5 mb-3">
+                  Trending Movies
+                </Text>
+
+                <FlatList
+                  data={trendingMovies}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  horizontal
+                  className="mb-4 mt-3"
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
+              </View>
+            )}
 
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
