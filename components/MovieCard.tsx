ad340a461c292093685d21242f92b410b5fc1598
rm-reins <rm_reins@icloud.com>
2025-12-11 00:35:36 +0100

    fix: update movie link path and display vote average with proper formatting

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -4,7 +4,7 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -19,7 +19,7 @@ const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie
           <View className="flex-row items-center gap-x-1">
             <Ionicons name="star" color="#FCBF49" size={16} />
             <Text className="text-xs text-white font-bold uppercase">
-              {vote_average.toFixed(1)}
+              {vote_average.toFixed(1)}/10
             </Text>
           </View>
           <Text className="text-xs text-light-300 font-medium">
