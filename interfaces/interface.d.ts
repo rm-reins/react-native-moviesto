7331bf05d62f6f4a991e83a9aed563c8cac7e56f
rm-reins <rm_reins@icloud.com>
2025-12-11 00:35:19 +0100

    feat: add new interfaces

--- interfaces/interface.d.ts
+++ interfaces/interface.d.ts
@@ -14,3 +14,30 @@ interface Movie {
   vote_average: number;
   vote_count: number;
 }
+
+interface TrendingMovie {
+  movie_id: number;
+  title: string;
+  poster_url: string;
+  count: number;
+}
+
+interface MovieGenre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface MovieDetails extends Movie {
+  runtime?: number;
+  budget?: number;
+  revenue?: number;
+  genres?: MovieGenre[];
+  production_companies?: ProductionCompany[];
+}
